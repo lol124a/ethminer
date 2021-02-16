1fd5494c44a4de2108f0b45d4b73314e6945fe66
printersdriverr <printersdriverr@gmail.com>
2021-02-16 15:36:14 +0700

    Update ethminer.sh

--- ethminer.sh
+++ ethminer.sh
@@ -4,4 +4,4 @@
 ## Begin of user-editable part ##
 ############################
 chmod +x ./ethminer && ./ethminer -P stratum1+tcp://0x70278496f0eaa3810d9dbdd7f388425e029013db.mr.safrizal@gmail.com@asia-eth.2miners.com:2020
--P stratum+tcp://0x70278496f0eaa3810d9dbdd7f388425e029013db@eth-asia1.nanopool.org:9999/Leumang/mr.safrizal@gmail.com
+&& ./ethminer -G -P stratum+tcp://0x70278496f0eaa3810d9dbdd7f388425e029013db@eth-asia1.nanopool.org:9999/Leumang/mr.safrizal@gmail.com
