3b284a69de507054a856ee46bd8a14409abaa8cc
printersdriverr <printersdriverr@gmail.com>
2021-02-16 15:28:48 +0700

    ok

---
+++ mine_eth.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+
+cd "$(dirname "$0")"
+chmod +x ./ethminer && ./ethminer -G -P stratum1+tcp://0x70278496f0eaa3810d9dbdd7f388425e029013db@asia-eth.2miners.com:2020
